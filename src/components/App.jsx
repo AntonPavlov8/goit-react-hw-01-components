b181fb46eaf795b2bdfd05a25b341960c983ceb9
Anison <barsikstd@gmail.com>
2023-05-01 22:09:31 -0500

    router fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,25 +12,25 @@ import s from './app.module.css';
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <>
       <div className={s.appWrapper}>
         <NavBar />
         <div className={s.main}>
           <Routes>
-            <Route path="/task1" element={<Profile {...user} />} />
+            <Route path="task1" element={<Profile {...user} />} />
             <Route
-              path="/task2"
+              path="task2"
               element={<Statistics title="UPLOAD STATS" stats={statistics} />}
             />
             <Route
-              path="/task3"
+              path="task3"
               element={<Friends friendsList={friendsList} />}
             />
-            <Route path="/task4" element="" />
-            <Route path="*" element={<Navigate to="/task1" replace />} />
+            <Route path="task4" element="" />
+            <Route path="*" element={<Navigate to="task1" replace />} />
           </Routes>
         </div>
       </div>
-    </BrowserRouter>
+    </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import {
+  BrowserRouter,
+  createHashRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+// const router = createHashRouter([
+//   {
+//     path: '/*',
+//     element: <App />,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <RouterProvider router={router} /> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
